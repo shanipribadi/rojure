dff29860413d33d5a2a27d15d99e846eae443b25
Carsten Behring <carsten.behring@gmail.com>
2017-09-28 20:30:26 +0200

    upgraded other deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :description "Clojure/R integration using rosuda Rserve"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [incanter/incanter-core "1.9.1"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [org.rosuda.REngine/REngine "2.1.0"]
-                 [org.clojure/core.incubator "0.1.3"]
+                 [org.clojure/core.incubator "0.1.4"]
                  [org.rosuda.REngine/Rserve "1.8.1"]]
 
   :profiles {:deployer {:repositories [["snapshots" {:url "file:///var/go/mvn-repo/"
